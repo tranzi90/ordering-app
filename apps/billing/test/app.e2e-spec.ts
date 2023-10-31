ae28a9f762844efe807f20748eab408571d8c950
denis_levchenko <tranzi90@gmail.com>
2023-10-31 23:27:14 +0300

    core code finished

--- apps/billing/test/app.e2e-spec.ts
+++ apps/billing/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { BillingModule } from './../src/billing.module';
+import { BillingModule } from '../src/billing.module';
 
 describe('BillingController (e2e)', () => {
   let app: INestApplication;
--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@app/common';
+import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersService } from './orders.service';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createOrder(@Body() request: CreateOrderRequest, @Req() req: any) {
+    return this.ordersService.createOrder(request, req.cookies?.Authentication);
+  }
+
   @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  async getOrders() {
+    return this.ordersService.getOrders();
   }
 }
